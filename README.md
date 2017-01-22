fa45079f0b25d99eb72154bd730e7740be44ef20
Daniel Harte <daniel@harte.me>
2017-01-22 23:10:06 +0000

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# chip-scripts
-Various bash scripts for CHIP
+# Chip Scripts
+Various bash scripts for CHIP (headless mode)
+
+#### CHIP flashing Instructions
+1. Connect the FEL - GND wire.
+2. Flash the CHIP with headless from the Chrome flasher at http://flash.getchip.com/.
+3. Power off the CHIP (hold button for 7 seconds).
+4. Remove FEL - GND wire.
+5. Power on the CHIP (hold button for 1 second).
+6. Wait for CHIP to boot.
+7. Launch putty and connect to COM port (find with Device Manager).
+8. Connect to you home wifi internet with ```nmcli d wifi connect "Netgear" password "12345678" ifname wlan0```
+9. Use `ifconfig` to find the CHIP's IP address
+10. Exit putty
+11. You can now connect to the CHIP over the local network (using putty or `ssh`)
+
+#### Update
+```apt update; apt upgrade -y```
+
+#### Download and run Chip Scripts
+```
+curl -sL https://raw.githubusercontent.com/norgeous/chip-scripts/master/cs.sh | bash -
+```
---
+++ first.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# CHIP customiser / first run
+# Author: norgeous
+# from: https://bbs.nextthing.co/t/a-few-things-that-a-new-chip-pocketchip-owner-should-do-know-and-try/13803
+# tested with 4.4 headless image
+
+# must run as ROOT
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# Update
+if (whiptail --title "Update" --yesno "Perform a system update?\n(internet connection required)" 8 40) then
+  apt update
+  apt upgrade -y
+  apt-get autoremove --purge -y
+fi
+
+# hostname
+CURRENTHOST=`hostname`
+NEWHOST=$(whiptail --title "Hostname" --inputbox "\nEnter new hostname:" 9 40 $CURRENTHOST 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+  sed -i "s/$CURRENTHOST/$NEWHOST/g" "/etc/hosts"
+  sed -i "s/$CURRENTHOST/$NEWHOST/g" "/etc/hostname"
+  hostname $NEWHOST
+  echo "The new hostname is '$NEWHOST'"
+fi
+
+# change 1000 username and set new password
+CURRENTUSER=`cat /etc/passwd | grep 1000 | cut -d: -f1`
+NEWUSER=$(whiptail --title "User" --inputbox "\nEnter new username for UID 1000:" 9 40 $CURRENTUSER 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+  usermod -l "$NEWUSER" -d "/home/$NEWUSER" -m "$CURRENTUSER"
+  echo "Enter password for the user '$NEWUSER':"
+  passwd "$NEWUSER"
+fi
+
+# disable root login, other user can still use sudo
+if (whiptail --title "Root Password" --yesno "Remove root password?" 8 40) then
+  passwd -dl root
+fi
+
+# Locale amd Timezone
+if (whiptail --title "Locale amd Timezone" --yesno "Install and configure Locales amd Timezones?" 8 40) then
+  apt install -y locales
+  dpkg-reconfigure locales
+  dpkg-reconfigure tzdata
+fi
+
+# Enable ll command
+if (whiptail --title "Enable ll command" --yesno "Enable ll command?" 8 40) then
+
+  # ll for root
+  sed -i "s/# export LS_OPTIONS='--color=auto'/export LS_OPTIONS='--color=auto -haX --group-directories-first'/g" "/root/.bashrc"
+  sed -i "s/# alias ll/alias ll/g" "/root/.bashrc"
+
+  # ll for user 1000
+  sed -i "s/# export LS_OPTIONS='--color=auto'/export LS_OPTIONS='--color=auto -haX --group-directories-first'/g" "/home/$NEWUSER/.bashrc"
+  sed -i "s/# alias ll/alias ll/g" "/home/$NEWUSER/.bashrc"
+
+fi
+
+# Swappiness
+if (whiptail --title "Swappiness" --yesno "Protect NAND by reducing swappiness to 10?" 8 40) then
+  if [ $(cat /etc/sysctl.conf | grep vm.swappiness | wc -l) -eq 0 ]; then
+
+cat <<EOF >> /etc/sysctl.conf
+#
+# protect NAND by reducing swappiness to 10
+vm.swappiness = 10
+EOF
+
+  fi
+  echo 10 > /proc/sys/vm/swappiness
+fi
