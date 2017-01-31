b9dfd6b7b0b9f9108f206519552f10dc12758c08
Daniel Harte <daniel@harte.me>
2017-01-31 01:22:06 +0000

    formatting

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Update
-if (whiptail --title "Update" --yesno "Perform the following commands?\n* apt update\n* apt upgrade -y\n* apt-get autoremove --purge -y\n(internet connection required)" 15 46) then
+if (whiptail --title "Update Packages" --yesno "\
+Please make sure you have an internet connection already configured.\
+\n\
+\n\
+Perform the following commands?\n\
+\n\
+* apt update\n\
+* apt upgrade -y\n\
+* apt-get autoremove --purge -y\n\
+" 15 46) then
   apt update
   apt upgrade -y
   apt-get autoremove --purge -y
